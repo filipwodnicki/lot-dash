9893caf993a423219525def2d4c7a41f4c861eed
bananalytics <31481575+bananalytics@users.noreply.github.com>
2018-08-17 17:42:13 +0200

    Fresh app created

---
+++ README.md
@@ -0,0 +1,3 @@
+# lot-dash
+
+This is a Flask app that displays a dashboart of LOT Polish Airlines transatlantic routes.
---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+# app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['LOG_TO_STDOUT'] = os.environ.get('LOG_TO_STDOUT')
+
+if not app.debug and not app.testing:
+	if app.config['LOG_TO_STDOUT']:
+		stream_handler = logging.StreamHandler()
+		stream_handler.setLevel(logging.INFO)
+		app.logger.addHandler(stream_handler)
+	else:
+		if not os.path.exists('logs'):
+			os.mkdir('logs')
+		file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+		file_handler.setFormatter(logging.Formatter(
+				'%(asctime)s %(levelname)s: %(message)s '
+				'[in %(pathname)s:%(lineno)d]'))
+		file_handler.setLevel(logging.INFO)
+		app.logger.addHandler(file_handler)
+
+		app.logger.setLevel(logging.INFO)
+		app.logger.info('Microblog startup')
+
+from app import routes
