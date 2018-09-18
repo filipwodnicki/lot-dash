e99fc29438fd9f7079e2768ced5f99667277ef97
bananalytics <31481575+bananalytics@users.noreply.github.com>
2018-09-18 14:30:41 +0200

    Removing all files in .gitignore

--- db/readcsv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-engine = create_engine('sqlite:////Users/Filip/code/python/lot-dash/app.db')
-
-df = pd.read_csv('/Users/Filip/code/python/lot-dash/db/test.csv')
-df.to_sql('user',con=engine, if_exists='append',index=False)
